7f563e29d9d20a80aaf909baf1187ad5477a0d51
dago22d <dg.an.mm@gmail.com>
2019-10-29 15:22:33 -0600

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'fdb17.awardspace.net');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', '2457615_inventarios');//Usuario de tu base de datos
+define('DB_PASS', '1nv3nt4r10s');//Contraseña del usuario de la base de datos
+define('DB_NAME', '2457615_inventarios');//Nombre de la base de datos
+?>
